2e4d5ec0f3555d0362177706cc1759142d1325bc
Muhammet Bozkurt <mubozk@gmail.com>
2022-08-05 10:11:10 +0300

    section 8 boss challenge done/ section 6 debug

---
+++ Section 8/magic-8_ball_flutter/lib/main.dart
@@ -0,0 +1,61 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+      ),
+    );
+
+////////////////////////////////////////////////////////
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text('ASK ME ANYTHING'),
+        titleTextStyle: TextStyle(color: Colors.red.shade900),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+////////////////////////////////////////////////////////
+
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  var wizardsAnswer = 1;
+
+  void randomize() {
+    setState(() {
+      wizardsAnswer = Random().nextInt(4) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Expanded(
+          child: FlatButton(
+            onPressed: () {
+              randomize();
+            },
+            child: Image.asset('images/ball$wizardsAnswer.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}

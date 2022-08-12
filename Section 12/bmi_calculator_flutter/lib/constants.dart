e142d2cf93cee860d7bc1a780ac1e0025d449c64
mubozk <mubozkt@gmail.com>
2022-08-12 10:58:30 +0300

    input page completed

--- Section 12/bmi_calculator_flutter/lib/constants.dart
+++ Section 12/bmi_calculator_flutter/lib/constants.dart
@@ -3,7 +3,12 @@ import 'package:flutter/material.dart';
 const Color kBottomContainerColour = Color(0xFFEB1555);
 const Color kActiveCardColour = Color(0xFF1D1E33);
 const Color kInactiveCardColour = Color(0xFF111328);
+const Color thumbColor = Color(0xFFEB1555);
+const Color overlayColor = Color(0x29eb1555);
+const Color activeTrackingColor = Color(0xFFEB1555);
 const double kBottomContainerHeight = 80.0;
+
+
 
 final kLabelTextStyle = TextStyle(
   color: Color(0xFF8D8E98),
--- Section 12/bmi_calculator_flutter/lib/input_page.dart
+++ Section 12/bmi_calculator_flutter/lib/input_page.dart
@@ -14,6 +14,8 @@ enum Gender { male, female }
 class _InputPageState extends State<InputPage> {
   Gender? selectedGender;
   int height = 170;
+  int weight = 60;
+  int age = 25;
 
   @override
   Widget build(BuildContext context) {
@@ -88,9 +90,9 @@ class _InputPageState extends State<InputPage> {
                       thumbShape:
                           (RoundSliderThumbShape(enabledThumbRadius: 15)),
                       overlayShape: RoundSliderOverlayShape(overlayRadius: 30),
-                      thumbColor: Color(0xFFEB1555),
-                      overlayColor: Color(0x29eb1555),
-                      activeTrackColor: Color(0xFFEB1555),
+                      thumbColor: thumbColor,
+                      overlayColor: overlayColor,
+                      activeTrackColor: activeTrackingColor,
                     ),
                     child: Slider(
                       value: height.toDouble(),
@@ -115,13 +117,89 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                     child: ReusableCard(
                   colour: kInactiveCardColour,
-                  cardChild: (Column()),
+                  cardChild: (Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'Weight',
+                        style: kLabelTextStyle,
+                      ),
+                      Text(
+                        weight.toString(),
+                        style: kNumberTextStyle,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          AdjustableIconButton(
+                            icon: Icon(FontAwesomeIcons.minus),
+                            onPress: () {
+                              print('weight--');
+                              setState(() {
+                                weight --;
+                              });
+                            },
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          AdjustableIconButton(
+                            icon: Icon(FontAwesomeIcons.plus),
+                            onPress: () {
+                              print('weight++');
+                              setState(() {
+                                weight ++;
+                              });
+                            },
+                          ),
+                        ],
+                      )
+                    ],
+                  )),
                 )),
                 Expanded(
                     child: ReusableCard(
-                  colour: kInactiveCardColour,
-                  cardChild: (Column()),
-                )),
+                      colour: kInactiveCardColour,
+                      cardChild: (Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            'AGE',
+                            style: kLabelTextStyle,
+                          ),
+                          Text(
+                            age.toString(),
+                            style: kNumberTextStyle,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              AdjustableIconButton(
+                                icon: Icon(FontAwesomeIcons.minus),
+                                onPress: () {
+                                  print('age--');
+                                  setState(() {
+                                    age --;
+                                  });
+                                },
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              AdjustableIconButton(
+                                icon: Icon(FontAwesomeIcons.plus),
+                                onPress: () {
+                                  print('age++');
+                                  setState(() {
+                                    age ++;
+                                  });
+                                },
+                              ),
+                            ],
+                          )
+                        ],
+                      )),
+                    )),
               ],
             ),
           ),
@@ -133,6 +211,25 @@ class _InputPageState extends State<InputPage> {
               child: Text('abc')),
         ],
       ),
+    );
+  }
+}
+
+class AdjustableIconButton extends StatelessWidget {
+  AdjustableIconButton({required this.icon,required this.onPress});
+
+  final Icon icon;
+  final VoidCallback onPress; // instead of a func
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      constraints: BoxConstraints.tightFor(height: 56, width: 56),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+      fillColor: Color(0xFF4C4F5E),
+      elevation: 11,
+      child: icon,
+      onPressed: onPress,
     );
   }
 }

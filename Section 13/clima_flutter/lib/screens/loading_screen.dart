109e878fd188ca61d9bc41770a41fd578fa40e24
mubozk <mubozk@icloud.com>
2022-08-14 18:57:14 +0300

    initial commit

--- Section 13/clima_flutter/lib/screens/loading_screen.dart
+++ Section 13/clima_flutter/lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:clima_flutter/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
@@ -13,6 +14,12 @@ class _LoadingScreenState extends State<LoadingScreen> {
     // TODO: implement initState
     super.initState();
     getLocation();
+    print('get triggered');
+  }
+
+  void getData () async {
+    Future<http.Response> response = http.get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    print(response);
   }
 
   void getLocation() async{
@@ -24,6 +31,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
